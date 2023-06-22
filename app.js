53d234fb3aed6c9a54e337362f88f70680267637
Vladimir Garmash <djonblack.mail@gmail.com>
2023-06-22 19:29:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import config from 'config';
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: config.get('OPENAI_KEY'),
+});
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.urlencoded( { extended: true } ));
+
+app.get('/', (_, res) => {
+    res.render('index');
+});
+
+app.post('/', async(req, res) => {
+    const prompt = req.body.prompt;
+    const size = req.body.size ?? '512x512';
+    const number = req.body.number ?? '1';
+
+    // console.log(prompt, size, number);
+
+    try {
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: Number(number),
+            size: size, 
+        });
+
+        console.log(response);
+
+        res.render('index', {
+            images: response.data.data
+        });
+    } catch (e) {
+        res.render('index', {
+            error: e.message,
+        })
+    }
+    
+});
+
+app.listen(3000, () => console.log('Server running...'));
